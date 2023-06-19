c6af19d135895e9a68fc8bed2c1c73596115b809
Lukas Hinterreiter <90035514+luhi-DT@users.noreply.github.com>
2023-06-19 15:31:11 +0200

    Adapt workflow

--- hack/build/ci/get-image-digest.sh
+++ hack/build/ci/get-image-digest.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 digest=$(skopeo inspect docker://"${IMAGE}" --format "{{.Digest}}")
+digest_value=$(echo ${digest} | cut -d ":" -f 2)
 echo "digest=${digest}">> "$GITHUB_OUTPUT"
+echo "digest_value=${digest_value}">> "$GITHUB_OUTPUT"
 
